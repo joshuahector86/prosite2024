126103bb1e9375d97536b1c91815e8eff6ac7b22
Dimitri Hector <joshuahector86@gmail.com>
2024-03-14 01:39:08 -0400

    adding emailjs to form

--- src/components/shared/ContactMeFlyOut/ContactMeForm.tsx
+++ src/components/shared/ContactMeFlyOut/ContactMeForm.tsx
@@ -12,6 +12,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { toast } from "@/components/ui/use-toast";
+import emailjs from "@emailjs/browser";
 
 export const ContactMeForm = () => {
   const formSchema = z.object({
@@ -20,7 +21,6 @@ export const ContactMeForm = () => {
     message: z.string().min(2).max(100),
   });
 
-  // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -33,20 +33,24 @@ export const ContactMeForm = () => {
   async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
       await formSchema.parseAsync(values);
-      console.log(values);
+      emailjs.send(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        values,
+        import.meta.env.VITE_EMAILJS_USER_ID
+      );
       toast({
         description:
           "The form was submitted successfully! Thanks for reaching out!",
       });
     } catch (error) {
-      console.log("it didnt work", error);
+      console.log("Error:", error);
       toast({
         description:
           "Please check the form fields and make corrections where needed.",
       });
     }
   }
-
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
